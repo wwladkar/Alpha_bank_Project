9178e6750f0d4070e4334b7fc0b0f438db31db69
Vladimir Karpushkin <114829633+wwladkar@users.noreply.github.com>
2024-03-22 17:34:31 +0300

    Update test_ovn_happy_path_page.py

--- test_ovn_happy_path_page.py
+++ test_ovn_happy_path_page.py
@@ -7,7 +7,7 @@ from pages.swift_page import SwiftPage
 
 def test_ovn_happy_path(browser):
     login_page = LoginPage(browser)
-    login_page.login("VKarpushkin", "kbepY5uLy")
+    login_page.login("VKarpushkin", "********")
 
     assert browser.current_url == f" {LoginPage.ezdoc_url} #/" or f"{LoginPage.ezdoc_url}#/portal/dashboard/dashboard"
 
