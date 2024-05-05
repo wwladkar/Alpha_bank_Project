497ea186f5ee0a8f4e993be25b9f3d5858c1b314
Vladimir Karpushkin <wwladkar@gmail.com>
2024-05-05 13:17:38 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,28 +1,55 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as OptionsChrome
+from selenium.webdriver.firefox.options import Options as OptionsFirefox
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
 
 
 def pytest_addoption(parser):
-    parser.addoption('--stand',
+    parser.addoption('--browser_name',
                      action='store',
-                     default='dev',
-                     help="Choose stand: '--stand=test' or '--stand=dev'")
-
-
-def stand(request):
-    stand_name = request.config.getoption("stand")
-    return stand_name
+                     default='yandex',
+                     help='Choose browser: \'--browser_name=chrome\' or \'--browser_name=firefox\' or '
+                          '\'--browser_name=yandex\'')
+    parser.addoption('--language',
+                     action='store',
+                     default='en',
+                     help="Choose language: '--language=en' or '--language=ru'")
 
 
 @pytest.fixture(scope="function")
-def browser():
-    options = OptionsChrome()
-    options.add_argument("start-maximized")
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),
-                              options=options)
-    yield driver
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("language")
+
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = OptionsChrome()
+        options.add_argument("start-maximized")
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),
+                                   options=options)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        options_firefox = OptionsFirefox()
+        options_firefox.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()),
+                                    options=options_firefox)
+    elif browser_name == "yandex":
+        print("\nstart Yandex browser for test..")
+        yandex_path = r"C:\Users\User\Downloads\yandexdriver-24.4.1.918-linux\yandexdriver"  # path to YandexDriver
+        yandex_options = webdriver.ChromeOptions()
+        yandex_options.binary_location = yandex_path
+        yandex_options.add_argument("start-maximized")
+        yandex_options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),
+                                   options=yandex_options)
+    else:
+        raise pytest.UsageError("--browser_name should be chrome, firefox, or yandex")
+    yield browser
     print("\nquit browser..")
-    driver.quit()
+    browser.quit()
