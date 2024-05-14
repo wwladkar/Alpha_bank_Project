4718a1bb7557f9a1a1ed5c9b4304106385ce430e
Vladimir Karpushkin <114829633+wwladkar@users.noreply.github.com>
2024-05-14 20:02:52 +0300

    Update test_login_page.py

--- test_login_page.py
+++ test_login_page.py
@@ -3,6 +3,6 @@ from pages.login_page import LoginPage
 
 def test_login(browser):
     login_page = LoginPage(browser)
-    login_page.login("VKarpushkin", "kbepY5uLy")
+    login_page.login("", "")
 
     assert browser.current_url == f" {LoginPage.ezdoc_url} #/" or f"{LoginPage.ezdoc_url}#/portal/dashboard/dashboard"
